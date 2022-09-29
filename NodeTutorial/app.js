4c173975574879ffb29e097f04d291f5ffeeaff2
Khaoula Benaicha <khaoulabenaicha036@gmail.com>
2022-09-29 23:58:40 +0200

    FirstHtmlPage

--- NodeTutorial/app.js
+++ NodeTutorial/app.js
@@ -1,9 +1,20 @@
-const http = require('http');
+const http = require('http')
 
-//require & response 
-const server = http.createServer((req, res)=>{
-    res.write('Welcome to our home page')
-    res.end()
+const server = http.createServer((req, res)  => {
+    if (req.url == '/') {   
+        res.end('welcome to our home page')
+    }
+    else if (req.url == '/about') {
+        res.end('Here is our short history')
+    }else {
+    res.end(`       
+    <h1>Oops!</h1>
+    <p>We can't seem to find the page you are looking for</p>
+    <a href="/">back home</a>
+     `)  
+    }        
 })
 
 server.listen(5000)
+
+
